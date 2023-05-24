6d7248c316d99f0bbb2645c1f4660a7b02cb7105
Artemy Gibson <artemygibson@gmail.com>
2023-05-24 08:18:17 -0700

    Write course class tests

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,43 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+  before do
+    @course = Course.new("Calculus", 2)  
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})  
+  end
 
+  it 'exists' do
+    expect(@course).to be_a(Course)
+  end
+
+  it 'has a name' do
+    expect(@course.name).to eq("Calculus")
+  end
+
+  it 'has a capacity' do
+    expect(@course.capacity).to eq(2)
+  end
+
+  it 'is initialized with no students' do
+    expect(@course.students).to eq([])
+  end
+
+  it 'has no students, so is not full' do
+    expect(@course.full?).to eq(false)
+  end
+
+  it 'can enroll students' do
+    @course.enroll(@student1)
+    @course.enroll(@student2)
+    expect(@course.students).to eq([@student1, @student2])
+  end
+
+  it 'cannot enroll more students than capacity' do
+    @course.enroll(@student1)
+    @course.enroll(@student2)
+    expect(@course.full?).to eq(true)
+    @course.enroll(Student.new({name: "Mitch", age: 24}))
+    expect(@course.students).to eq([@student1, @student2])
+  end
 end
