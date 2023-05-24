b9400e19016f366e7ee22aa93d09575e019b47dd
Artemy Gibson <artemygibson@gmail.com>
2023-05-24 09:29:03 -0700

    Add all_grades and students_in_range methods

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -21,4 +21,21 @@ class Gradebook
     students = self.list_all_students.values.flatten
     students.select {|student| student.grade < threshold}
   end
+
+  def all_grades
+    grade_list = Hash.new{|grade_list, course| grade_list[course] = []}
+    self.list_all_students.each do |course, students|
+      students.each do |student|
+        grade_list[course] << student.grade
+      end
+    end
+    grade_list
+  end
+
+  def students_in_range(min, max)
+    students = self.list_all_students.values.flatten
+    students.select do |student|
+      student.grade > min && student.grade < max
+    end
+  end
 end
