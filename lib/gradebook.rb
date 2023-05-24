45ced50a766f1c964a706da852827a84796250fb
Artemy Gibson <artemygibson@gmail.com>
2023-05-24 08:53:41 -0700

    Write gradebook class

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,24 @@
 class Gradebook
-  
+  attr_reader :instructor, :courses
+  def initialize(instructor, courses)
+    @instructor = instructor
+    @courses = courses
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    enrollment_list = {}
+    @courses.each do |course|
+      enrollment_list[course] = course.students
+    end
+    enrollment_list
+  end
+
+  def students_below(threshold)
+    students = self.list_all_students.values.flatten
+    students.select {|student| student.grade < threshold}
+  end
 end
